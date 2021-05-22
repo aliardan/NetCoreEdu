ead1ac52ea6237b39b4609a0586eb18d64b88fd6
aliardan <aliardanchik@gmail.com>
2021-05-22 18:48:37 +0300

    feat/Created RepositoryBase

---
+++ Microservices/RepositoryBase/RepositoryBase/RepositoryBase/IRepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryBase
+{
+    public interface IRepositoryBase<T>
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> CreateAsync(T entity);
+        Task<IEnumerable<T>> CreateMany(IEnumerable<T> entities);
+        Task<bool> Update(T entity);
+        Task<bool> UpdateMany(IEnumerable<T> entities);
+        Task<bool> Delete(Guid id);
+        Task<bool> DeleteMany(IEnumerable<Guid> id);
+        Task<bool> Restore(Guid id);
+        Task<bool> RestoreMany(IEnumerable<Guid> id);
+    }
+}
