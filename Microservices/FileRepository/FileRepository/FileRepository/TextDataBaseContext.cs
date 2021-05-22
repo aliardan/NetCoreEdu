98d024fd26f0c4eea04d00964fe42b7eb10990c3
aliardan <aliardanchik@gmail.com>
2021-05-22 19:54:32 +0300

    feat/Implementation of EF Context

---
+++ Microservices/FileRepository/FileRepository/FileRepository/TextDataBaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace FileRepository
+{
+    class TextDataBaseContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public TextDataBaseContext(IOptions<TextDataBaseOptions> options)
+        {
+            _connectionString = options.Value.ConnectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<File>();
+        }
+
+        public DbSet<File> Files { get; set; }
+    }
+}
---
+++ Microservices/FileRepository/FileRepository/FileRepository/TextDataBaseOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRepository
+{
+    class TextDataBaseOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}
