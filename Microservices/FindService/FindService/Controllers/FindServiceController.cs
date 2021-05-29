71f7c3f963d1bc659015590da6101beba15969ca
aliardan <aliardanchik@gmail.com>
2021-05-29 18:52:18 +0300

    feat/FindService logic implementation

---
+++ Microservices/FindService/FindService/Controllers/FindServiceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindService.Controllers
+{
+    [ApiController]
+    public class FindServiceController
+    {
+        private readonly FindService.FindService _findService;
+        public FindServiceController(FindService.FindService findService)
+        {
+            _findService = findService;
+        }
+
+        [HttpGet ("[controller]/{id}")]
+        public async Task<int> Find([FromRoute]Guid id, [FromQuery]string[] words)
+        {
+            return await _findService.Find(id, words);
+        }
+    }
+}
---
+++ Microservices/FindService/FindService/FindService/FindService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Threading.Tasks;
+using TextRepository;
+
+namespace FindService.FindService
+{
+    public class FindService
+    {
+        private readonly ITextRepository _textRepository;
+
+        public FindService(ITextRepository textRepository)
+        {
+            _textRepository = textRepository;
+        }
+
+        public async Task<int> Find(Guid id, string[] wordsForSearch)
+        {
+           var text = await _textRepository.GetByIdAsync(id);
+           var textWords = text.Content.Split().ToList();
+           int counter = 0;
+           var wordsForSearchHashSet = new HashSet<string>(wordsForSearch);
+
+           foreach (var textWord in textWords)
+           {
+               if (wordsForSearchHashSet.Contains(textWord))
+               {
+                   counter++;
+               }
+           }
+
+           return counter;
+        }
+    }
+}
