cc0acfcd38d19a463a9d81cbad8691daacbfdfcb
aliardan <aliardanchik@gmail.com>
2021-06-05 11:04:12 +0300

    feat/WeatherService Initial implementation

---
+++ WeatherService/WeatherService/WeatherService/WeatherService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherService.Models;
+
+namespace WeatherService.WeatherService
+{
+    public class WeatherService
+    {
+        public async Task<CityTemperature> GetCityTemperature(string cityName, Metric metric)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CityWind> GetCityWind(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<WeatherForecast>> GetCityForecast(string cityName, Metric metric)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
