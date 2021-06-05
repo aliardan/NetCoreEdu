093ede25609824040158fff6a80569fb04dfd5de
aliardan <aliardanchik@gmail.com>
2021-06-05 10:46:57 +0300

    feat/WeatherForecast model implementation

---
+++ WeatherService/WeatherService/Models/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WeatherService.Models
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public string City { get; set; }
+
+        public int Temperature { get; set; }
+
+        public Metric Metric { get; set; }
+    }
+}
